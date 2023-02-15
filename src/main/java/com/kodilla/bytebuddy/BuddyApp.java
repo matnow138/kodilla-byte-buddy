80665b6810450b7f3cae76749afb947846b6aad4
unknown <mateuszp.nowak@gmail.com>
2023-02-15 17:04:16 +0100

    byte-buddy

--- src/main/java/com/kodilla/bytebuddy/BuddyApp.java
+++ src/main/java/com/kodilla/bytebuddy/BuddyApp.java
@@ -1,2 +1,21 @@
-package com.kodilla.bytebuddy;public class BuddyApp {
+package com.kodilla.bytebuddy;
+
+import net.bytebuddy.ByteBuddy;
+import net.bytebuddy.dynamic.loading.ClassLoadingStrategy;
+import net.bytebuddy.implementation.FixedValue;
+import net.bytebuddy.matcher.ElementMatcher;
+import net.bytebuddy.matcher.ElementMatchers;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.List;
+
+public class BuddyApp {
+
+    public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        List<Book> books = BooksGenerator.generate(100);
+        BooksFilter booksFilter = new BooksFilter(books);
+        List<Book> filteredBooks = booksFilter.onlyBooksOlderThan(20);
+        System.out.println(filteredBooks.size());
+    }
+
 }
